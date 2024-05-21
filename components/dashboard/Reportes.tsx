c4f2c6f65c9c7e1c7ff7ccc2d9896794ee9a58a9
hayyrs66 <rayrtsx@proton.me>
2024-05-20 20:44:08 -0600

    Add Reportes component for dashboard page

---
+++ components/dashboard/Reportes.tsx
@@ -0,0 +1,30 @@
+import { dataOverview } from "@/app/constants";
+
+function Reportes() {
+  return (
+    <>
+      {/* overview  */}
+      <div className="w-full h-full flex gap-5">
+        {dataOverview.map((item, index) => (
+          <div
+            key={index}
+            className="w-fit min-w-60 mt-10 border border-white/15 rounded-md
+            p-5"
+          >
+            <div className="flex w-full justify-between gap-5">
+              <h3 className="text-white/50 text-sm tracking-tight font-medium">
+                {item.title}
+              </h3>
+              <div>{item.icon && <item.icon className="text-white/50" />}</div>
+            </div>
+            <h4 className="text-white text-2xl tracking-tight font-bold">
+              Q{item.total}
+            </h4>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Reportes;
