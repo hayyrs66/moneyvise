02035dfff3e006bd7d3e0b71a0829e9c827f2469
hayyrs66 <rayrtsx@proton.me>
2024-05-20 20:42:00 -0600

    Add dashboard page component

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import DashboardManager from "@/components/DashboardManager";
+
+export default function Page() {
+    return (
+      <main className="relative w-full h-full flex">
+        <div className="w-full h-full">
+          <DashboardManager />
+        </div>
+      </main>
+    );
+  }
+  
