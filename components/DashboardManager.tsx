1c2787a10c0543bc9a47aee4ce5dc0b11d7b5b58
hayyrs66 <rayrtsx@proton.me>
2024-05-20 21:36:44 -0600

    feat: Add tab for dashboard navigation

--- components/DashboardManager.tsx
+++ components/DashboardManager.tsx
@@ -1,35 +1,22 @@
 "use client";
-
 import { useSearchParams } from "next/navigation";
-import { dataOverview } from "@/app/constants";
-import { MdAccountBalance } from "react-icons/md";
-import { FaMoneyBill1Wave } from "react-icons/fa6";
-import { useEffect } from "react";
+import General from "./dashboard/General";
+import Reportes from "./dashboard/Reportes";
+import Pagos from "./dashboard/Pagos";
+import Notificaciones from "./dashboard/Notificaciones";
 
 const DashboardManager = () => {
   const searchParams = useSearchParams();
   const tab = searchParams.get("tab");
 
-  const renderTab = () => {
-    switch (tab) {
-      case "general":
-        return <h1>General</h1>;
-      case "reportes":
-        return <h1>Reportes</h1>;
-      case "pagos":
-        return <h1>Pagos</h1>;
-      case "notificaciones":
-        return <h1>Notificaciones</h1>;
-      default:
-        return <h1>General</h1>;
-    }
+  const tabComponents: { [key: string]: JSX.Element } = {
+    general: <General />,
+    reportes: <Reportes />,
+    pagos: <Pagos />,
+    notificaciones: <Notificaciones />,
   };
 
-  return (
-    <section className="w-full h-full">
-      <div className="w-full h-full border border-white/15 rounded-md"></div>
-    </section>
-  );
+  return tabComponents[tab ?? "general"] || null;
 };
 
 export default DashboardManager;
