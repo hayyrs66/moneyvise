d8880682401fc3a39a22621b7f19113efbcf9b90
hayyrs66 <rayrtsx@proton.me>
2024-05-23 22:54:39 -0600

    refactor: Update PrimaryButton component to include user session and improve styles

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,43 +1,47 @@
 "use client";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { SlArrowDown } from "react-icons/sl";
 
 const PrimaryButton = () => {
   const { data: session } = useSession();
 
   if (session) {
     return (
-      <button
-        className="relative inline-flex  overflow-hidden rounded-xl p-[1px] focus:outline-none"
-        onClick={() => signOut(
-          { callbackUrl: "http://localhost:3000/" }
-        )}
-      >
-        <span className="absolute inset-[-1000%] animate-[spin_15s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,transparent_0%,#FFFFFF_40%,transparent_45%)]"></span>
-        <span
-          className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-xl
-            px-5 py-1 text-sm font-medium text-gradient-effect backdrop-blur-2xl `}
-        >
-          Sign Out
-        </span>
-      </button>
+      <div className="flex justify-end items-center gap-5 w-full h-full">
+        <button className="flex items-center gap-2 border border-white/20 rounded-md p-2">
+          {session ? (
+            <img
+              src={session.user?.image as string}
+              alt={`${session.user?.name} profile picture`}
+              width={20}
+              height={20}
+              className="rounded-full"
+            />
+          ) : (
+            <div className="w-[20px] h-[20px] rounded-full bg-white"></div>
+          )}
+
+          <span className="text-white text-sm tracking-tight font-medium mr-4">
+            {session.user?.name ?? "User"}
+          </span>
+          <SlArrowDown className="text-white/50" width={5} height={5} />
+        </button>
+      </div>
     );
   }
   return (
-    <button
-      className="relative inline-flex  overflow-hidden rounded-xl p-[1px] focus:outline-none "
-      onClick={() => signIn(
-        "google",
-        { callbackUrl: `${window.location.origin}/dashboard` }
-      )}
-    >
-      <span className="absolute inset-[-1000%] animate-[spin_15s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,transparent_0%,#FFFFFF_40%,transparent_45%)]"></span>
-      <span
-        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-xl
-            px-5 py-1 text-sm font-medium text-white backdrop-blur-2xl border-[0.5px] border-white/15 `}
+    <div className="flex justify-end items-center gap-5 w-full h-full">
+      <button
+        onClick={() =>
+          signIn("google", { callbackUrl: "http://localhost:3000/dashboard" })
+        }
+        className="w-fit px-[1.5rem] py-[0.3rem] rounded-md innershadow "
       >
-        Sign in
-      </span>
-    </button>
+        <span className="text-white text-sm tracking-tight font-medium whitespace-nowrap">
+          Sign in
+        </span>
+      </button>
+    </div>
   );
 };
 
