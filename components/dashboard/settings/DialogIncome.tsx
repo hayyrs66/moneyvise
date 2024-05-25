55da90345efff674c47fbed4195b91c6f060360e
hayyrs66 <rayrtsx@proton.me>
2024-05-24 22:47:45 -0600

    refactor: Add DialogIncome component for displaying income dialog form

---
+++ components/dashboard/settings/DialogIncome.tsx
@@ -0,0 +1,68 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { ReactNode } from "react";
+import { SelectClasification } from "./Clasifications";
+
+export function DialogIncome({ children }: { children: ReactNode }) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="sm:max-w-[425px] dark">
+        <DialogHeader>
+          <DialogTitle className="text-white">Agregar Ingreso</DialogTitle>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right text-white">
+              Nombre
+            </Label>
+            <Input
+              id="name"
+              placeholder="Ej. Pago de nómina"
+              className="col-span-3 text-white/80"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right text-white">
+              Monto
+            </Label>
+            <Input
+              id="name"
+              placeholder="Ej. 1000"
+              className="col-span-3 text-white/80"
+            />
+          </div>
+          <div className="flex items-center gap-4">
+            <Label htmlFor="username" className="text-right text-white">
+              Descripción
+            </Label>
+            <Textarea
+              className="resize-none w-full text-white/80"
+              placeholder="Ej. Pago de nómina Eleks"
+            />
+          </div>
+          <div className="flex items-center gap-4">
+            <Label htmlFor="username" className="text-right text-white">
+              Clasificación
+            </Label>
+            <SelectClasification />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Guardar</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
